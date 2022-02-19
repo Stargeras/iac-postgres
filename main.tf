5b5dfa4d05ef24685face467c233d247fd36f0e5
Stargeras <64556423+Stargeras@users.noreply.github.com>
2022-02-19 18:19:25 -0500

    fix syntax

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ module "postgres" {
     source = "./module-postgres"
     kubeconfig = "${var.kubeconfig}"
     postgres_password = "${var.postgres_password}"
-    storage_class = "${var.storage_class}
+    storage_class = "${var.storage_class}"
     postgres_namespace = "${var.postgres_namespace}"
 }
