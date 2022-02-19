5f829095f8c792b975eea1e4b666bf2c64675469
Stargeras <64556423+Stargeras@users.noreply.github.com>
2022-02-19 18:48:40 -0500

    fix new vars

--- main.tf
+++ main.tf
@@ -10,6 +10,8 @@ module "pgadmin" {
     source = "./module-pgadmin"
     kubeconfig = "${var.kubeconfig}"
     pgadmin_namespace = "${var.pgadmin_namespace}"
+    pgadmin_replicas = "${var.pgadmin_replicas}"
+    pgadmin_port = "${var.pgadmin_port}"
     pgadmin_image = "${var.pgadmin_image}"
     pgadmin_email = "${var.pgadmin_email}"
     pgadmin_password = "${var.pgadmin_password}"
